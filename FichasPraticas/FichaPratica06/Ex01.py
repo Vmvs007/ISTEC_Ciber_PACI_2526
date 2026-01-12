80a6b19fd6450eb223bfec07677d063baa027e5f
Vitor Santos <vmvs007@gmail.com>
2026-01-12 09:59:03 +0000

    Dicionarios

---
+++ FichasPraticas/FichaPratica06/Ex01.py
@@ -0,0 +1,11 @@
+nomeLido = input("Insira o nome: ")
+idadeLida = int(input("Insira a sua idade: "))
+cidadeLida = input("Insira a sua cidade:")
+
+dicionarioUtilizador = {
+    "nome": nomeLido,
+    "idade": idadeLida,
+    "cidade": cidadeLida
+}
+
+print(dicionarioUtilizador)
